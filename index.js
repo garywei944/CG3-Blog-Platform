e9b5860614d96ec1ec3d78bfb05168c1c18d08a4
Peter F. Klemperer <peter@peterklemperer.com>
2021-01-03 15:22:58 -0500

    menu and confirmation have working post

--- index.js
+++ index.js
@@ -5,9 +5,57 @@ const PORT = process.env.PORT || 5000
 
 express()
   .use(express.static(path.join(__dirname, 'public')))
+  .use(express.urlencoded({extended: true}))
   .get('/', function(req, res) {
     res.sendFile(path.join(__dirname + '/index.html'));
   })
+  .post('/order', (req, res) => {
+      const first_name = req.body.first;
+      const last_name = req.body.last;
+      let entree = req.body.entree;
+      let sideList = getSides(req.body);
+
+      if (validateMenu(first_name, last_name, entree, sideList)) {
+        res.redirect('/confirmation.html');
+      } else {
+        res.redirect('/menu.html');
+      }
+  })
+  .post('/confirm', (req, res) => {
+      console.log(req.body);
+        
+      res.redirect('/customerstatus.html');
+  })
+
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
 
 
+function validateMenu(first_name, last_name, entree, sideList) {
+    let valid = false;
+
+    if (first_name.length != 0 &&
+        last_name.length != 0 && 
+        entree != undefined && 
+        sides.length === 3) {
+        valid = true;
+    }
+
+    return valid;
+}
+
+function getSides(body) {
+    sides = [];
+    
+    if (body.item0 === "on")
+        sides.push("Corn Bread")
+    if (body.item1 === "on")
+        sides.push("Creamed Corn")
+    if (body.item2 === "on")
+        sides.push("Green Beans")
+    if (body.item3 === "on")
+        sides.push("Mashed Potatos")
+    if (body.item4 === "on")
+        sides.push("Baked Beans")
+
+    return sides;
+}
---
+++ public/menuvalidate.js
@@ -0,0 +1,8 @@
+// menuvalidate.js
+// Peter Klemperer
+// January 3, 2021
+
+let sidesValid = false;
+
+// TODO add in validation to make sure that exactly three sides were selected
+
