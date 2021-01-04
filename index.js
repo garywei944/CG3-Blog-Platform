2888a41147d851c5a8b4cc526902f906cdedde92
Peter F. Klemperer <peter@peterklemperer.com>
2021-01-04 15:14:11 -0500

    db now shows order table

--- index.js
+++ index.js
@@ -84,7 +84,7 @@ express()
   .get('/db', async (req, res) => {
     try {
       const client = await pool.connect();
-      const result = await client.query('SELECT * FROM test_table');
+      const result = await client.query('SELECT * FROM order_table');
       const results = { 'results': (result) ? result.rows : null};
       res.render('pages/db', results );
       client.release();
