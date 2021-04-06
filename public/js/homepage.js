860ae0d483b30dbeda324b43d72cd7468d10cc4f
garywei944 <33930674+garywei944@users.noreply.github.com>
2021-04-05 21:17:54 -0400

    Update URL in .js files and Add profile.html

--- public/js/homepage.js
+++ public/js/homepage.js
@@ -1,33 +1,33 @@
-$(document).ready(function (){
+$(document).ready(function () {
     let num = 1;
     //The content in each blog will be replaced with actual post when we are at the database step.
-    for (let i = 1; i < 7; i++){
+    for (let i = 1; i < 7; i++) {
         post = "<tr><td class='list'><a>Blog " + num + "</a></td></tr>";
         $('#posts').append(post);
         num++;
     }
 
     let wlNum = 1;
-    for(let i = 1; i < 5; i++){
+    for (let i = 1; i < 5; i++) {
         waitList = "<div class='wl'><a>Waitlist " + wlNum + "</a></div>";
         $('#later').append(waitList);
         wlNum++;
     }
 
-    $("#newpost").click(function (){
-        window.location.href = 'https://stark-tor-10041.herokuapp.com/postpage.html';
+    $("#newpost").click(function () {
+        window.location.href = 'postpage.html';
     });
 
-    $("#home").click(function (){
-        window.location.href = 'https://stark-tor-10041.herokuapp.com/homepage2.html';
+    $("#home").click(function () {
+        window.location.href = 'index.html';
     });
 
-    $(".list").click(function (){
-        window.location.href = 'https://stark-tor-10041.herokuapp.com/genglin_blog_page.html';
+    $(".list").click(function () {
+        window.location.href = 'genglin_blog_page.html';
     });
 
-    $(".wl").click(function (){
-        window.location.href = 'https://stark-tor-10041.herokuapp.com/genglin_blog_page.html';
+    $(".wl").click(function () {
+        window.location.href = 'genglin_blog_page.html';
     })
 
 });
