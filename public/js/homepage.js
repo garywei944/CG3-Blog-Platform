5eb80f74bba731391ea5f99f6618c6e16e94724c
genglinliu <44108482+genglinliu@users.noreply.github.com>
2021-04-19 16:05:37 -0400

    update blogpage hyperlinks

--- public/js/homepage.js
+++ public/js/homepage.js
@@ -24,7 +24,7 @@ $(document).ready(function () {
     }, 'json');
 
     $("#newpost").click(function () {
-        window.location.href = 'postpage.html';
+        window.location.href = 'post.html';
     });
 
     $("#home").click(function () {
