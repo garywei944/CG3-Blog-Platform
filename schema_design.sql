250fd1e15b5dee6b27dbb2e86a4c183ca351b0b7
genglinliu <44108482+genglinliu@users.noreply.github.com>
2021-04-19 18:11:59 -0400

    update commands for tables

--- schema_design.sql
+++ schema_design.sql
@@ -1,4 +1,6 @@
 
+-- create tables
+
 CREATE TABLE user_account (
     username VARCHAR(20), 
     pwd VARCHAR(20),
@@ -30,25 +32,28 @@ CREATE TABLE follow (
     FOREIGN KEY(follower_name) REFERENCES user_account(username)
 );
 
+-- populate tables with data
 
-INSERT INTO user_account values ('Alex', 123, 'fake_url1', 'hello I am Alex');
-INSERT INTO user_account values ('Chris', 456, 'fake_url2', 'hello I am Chris');
-INSERT INTO user_account values ('Devin', 789, 'fake_url3', 'hello I am Devin');
+INSERT INTO user_account values 
+    ('Alex', 123, 'fake_url1', 'hello I am Alex'),
+    ('Chris', 456, 'fake_url2', 'hello I am Chris'),
+    ('Devin', 789, 'fake_url3', 'hello I am Devin');
 
-INSERT INTO post (username, title, content) values ('Alex', 'post alex', 'story alex');
-INSERT INTO post (username, title, content) values ('Alex', 'post alex 2', 'story alex 2');
-INSERT INTO post (username, title, content) values ('Chris', 'post Chris', 'story Chris');
+INSERT INTO post (username, title, content) values 
+    ('Alex', 'post alex', 'story alex'),
+    ('Alex', 'post alex 2', 'story alex 2'),
+    ('Chris', 'post Chris', 'story Chris');
 
+INSERT INTO liked values 
+ ('Alex', 'post Chris'),
+ ('Chris', 'post alex');
 
+INSERT INTO follow values ('Alex', 'Chris'),
+    ('Chris', 'Alex'),
+    ('Alex', 'Devin');
 
-INSERT INTO liked values ('Alex', 'post Chris');
-INSERT INTO liked values ('Chris', 'post alex');
-
-INSERT INTO follow values ('Alex', 'Chris');
-INSERT INTO follow values ('Chris', 'Alex');
-INSERT INTO follow values ('Alex', 'Devin');
-
-ALTER TABLE follow ALTER COLUMN follower_name TYPE VARCHAR(20);
+-- debug: modify column type
+-- ALTER TABLE follow ALTER COLUMN follower_name TYPE VARCHAR(20);
 
 -- add post_id
 ALTER TABLE post ADD post_id VARCHAR(20);
@@ -58,7 +63,7 @@ UPDATE post SET post_id = '01' WHERE title = 'post alex';
 UPDATE post SET post_id = '02' WHERE title = 'post alex 2';
 UPDATE post SET post_id = '03' WHERE title = 'post Chris';
 
-
+-- dropping primary key attempt: didn't actually do it
 -- ALTER TABLE post DROP CONSTRAINT post_pkey;
 
 -- adding three posts
