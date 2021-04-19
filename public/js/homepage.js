3d46cd4d93e10d5dac19264803dc4e9d466c3b4a
GavinDuan513 <gavin-duan@qq.com>
2021-04-20 04:47:36 +0800

    Retrieve in Blogpage 1st time

--- public/js/homepage.js
+++ public/js/homepage.js
@@ -1,19 +1,4 @@
 $(document).ready(function () {
-    // let num = 1;
-    // //The content in each blog will be replaced with actual post when we are at the database step.
-    // for (let i = 1; i < 7; i++) {
-    //     post = "<tr><td class='list'><a>Blog " + num + "</a></td></tr>";
-    //     $('#posts').append(post);
-    //     num++;
-    // }
-
-    // let wlNum = 1;
-    // for (let i = 1; i < 5; i++) {
-    //     waitList = "<div class='wl'><a>Waitlist " + wlNum + "</a></div>";
-    //     $('#later').append(waitList);
-    //     wlNum++;
-    // }
-
     $.get('/homepage', function(data){
         data.results.forEach(function(item){
             let post = "<tr><td class='list'><a>" + item.title + "</a></td></tr>";
