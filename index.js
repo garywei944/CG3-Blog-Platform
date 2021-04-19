f16b95d16c68381dba2d83503839c0e6a7e12490
Lvpeipei <peihenglyu@gmail.com>
2021-04-19 18:01:23 -0400

    test db 13

--- index.js
+++ index.js
@@ -224,7 +224,7 @@ express()
       const client = await pool.connect();
       const result = await client.query("SELECT * FROM pg_tables WHERE tablename NOT LIKE 'pg_%' AND tablename NOT LIKE 'sql_%'");
       const result2 = await client.query("SELECT * FROM user_account");
-      const results = { 'results': (result) ? result.rows : null,'results2': (result2) ? result2.rows : null};
+      const results = {'results2': (result2) ? result2.rows : null,'results': (result) ? result.rows : null};
       res.render('pages/db', results );
       client.release();
     } catch (err) {
