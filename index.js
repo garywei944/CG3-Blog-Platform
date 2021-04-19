362a3befe6435900bced36cfd6d52dc5ef7e71b0
Lvpeipei <peihenglyu@gmail.com>
2021-04-19 17:05:58 -0400

    test db 11

--- index.js
+++ index.js
@@ -222,7 +222,7 @@ express()
   .get('/db', async (req, res) => {
     try {
       const client = await pool.connect();
-      const result = await client.query('SELECT * FROM pg_tables');
+      const result = await client.query('SELECT * FROM pg_tables WHERE tablename NOT LIKE "pg%" AND tablename NOT LIKE "sql_%"');
       const results = { 'results': (result) ? result.rows : null};
       res.render('pages/db', results );
       client.release();
