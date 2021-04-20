c69e4cf37cf48ebeff7707f1a305eda008565bb3
GavinDuan513 <gavin-duan@qq.com>
2021-04-20 21:44:45 +0800

    limit 6

--- public/js/homepage.js
+++ public/js/homepage.js
@@ -1,13 +1,8 @@
 $(document).ready(function () {
     $.get('/api/homepage', function(data){
-        let cnt = 0;
         data.results.forEach(function(item){
-            if(cnt > 5){
-                break;
-            }
             let post = "<tr><td class='list'><a>" + item.title + "</a></td></tr>";
             $('#posts').append(post);
-            cnt += 1;
         })
     }, 'json').done(function(){
         console.log($(".list"));
