438877fb4181347bbf14e2b80d584dd48f2cb712
genglinliu <44108482+genglinliu@users.noreply.github.com>
2021-04-24 14:30:56 -0400

    blogpage frontend ajax setup

--- public/js/blogpage.js
+++ public/js/blogpage.js
@@ -1,16 +1,53 @@
 
 $(document).ready(function() {
-
-    // TODO: change all of these to AJAX POST requests
-
+    
     // click on "like" - add 'this' user and 'this blog' to the database table 'liked'
-    $("#like").click(function() {
-        console.log("liked the post")
+    $('#like').click(function(){                
+
+        console.log("like button clicked")
+
+        const input = {
+            user_id: "this_user",
+            post_id: "this_post"
+        };
+    
+        console.log(input);
+    
+        $.ajax({
+            type: "POST",
+            url: "/api/blogpage_backend/",
+            data: input,
+        })
+        .done(function(data) {
+            // Successfully deleted entree
+        })
+        .fail(function(jqXHR) {
+            alert("error");
+        });
     });
 
     // click on "follow" - add 'this' user and 'this blogger' to the database table 'follows'
     $("#follow").click(function() {
-        console.log("followed the blogger")
+        console.log("follow button clicked")
+
+        const input = {
+            this_user_id: "this_user",
+            poster_user_id: "poster_user"
+        };
+    
+        console.log(input);
+    
+        $.ajax({
+            type: "POST",
+            url: "/api/blogpage_backend/",
+            data: input,
+        })
+        .done(function(data) {
+            // Successfully deleted entree
+        })
+        .fail(function(jqXHR) {
+            alert("error");
+        });
     });
 
 
@@ -19,27 +56,5 @@ $(document).ready(function() {
         alert("added to the read-later list")
     });
 });
-
-
-// $('#like').click(function(){                
-//     // POST a request with the JSON-encoded entree to the backend
-//     const input = {
-//         user_id: $("#entreeId").val(),
-//         post_id: $("#entreeName").val(),
-//     };
-//     console.log(input);
-
-//     $.ajax({
-//         type: "POST",
-//         url: "/api/entrees/",
-//         data: input,
-//     }).done(function(data) {
-//         // Successfully deleted entree
-//         $("form").trigger("reset");
-//         getAllEntrees();
-//     }).fail(function(jqXHR) {
-//         $("#error").html("The entree could not be deleted.");
-//     });
-// })
 
 
