cdd2e5bceab9d1d88a5e17ec761c930ca95452cb
genglinliu <44108482+genglinliu@users.noreply.github.com>
2021-04-24 00:46:51 -0400

    settting up TODOs

--- public/js/blogpage.js
+++ public/js/blogpage.js
@@ -1,26 +1,22 @@
 
 $(document).ready(function() {
 
-    // TODO: change all of these to AJAX requests
+    // TODO: change all of these to AJAX POST requests
 
-    // click on the circle profile pic => switch to profile page 
-    $(".circle").click(function(){
-        window.location.href='profile.html'; // switch to profile page instead (when we have one)
-    });
-
-    // click on "read later" => show alert("placed ")
-    $("#readLater").click(function() {
-        alert("added to the read-later list")
-    });
-
-    // click on "like"
+    // click on "like" - add 'this' user and 'this blog' to the database table 'liked'
     $("#like").click(function() {
         alert("liked the post")
     });
 
-    // click on "favorite"
+    // click on "follow" - add 'this' user and 'this blogger' to the database table 'follows'
     $("#follow").click(function() {
         alert("followed the blogger")
+    });
+
+
+    // click on "read later" - future task
+    $("#readLater").click(function() {
+        alert("added to the read-later list")
     });
 });
 
---
+++ routes/api/blogpage_backend.js
@@ -0,0 +1,6 @@
+
+// TODO: handle the two requests and insert them into database
+
+// click on "like" - add 'this' user and 'this blog' to the sql table 'liked'
+
+// click on "follow" - add 'this' user and 'this blogger' to the sql table 'follow'
