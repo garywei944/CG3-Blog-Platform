ba9df65c8e7592d0919a6eb0f858c55c0f0b1f4d
garywei944 <33930674+garywei944@users.noreply.github.com>
2021-04-25 09:10:48 -0400

    Working on the rich text editor

--- public/js/post.js
+++ public/js/post.js
@@ -1,8 +1,31 @@
-ClassicEditor
-    .create($("#editor")[0])
-    .then(editor => {
-        window.editor = editor;
-    })
-    .catch(error => {
-        console.error('There was a problem initializing the editor.', error);
-    });
+$(function () {
+    let $editor = $("#editor");
+
+    ClassicEditor
+        .create($editor[0], {
+            toolbar: ['heading', '|', 'bold', 'italic', 'link', 'bulletedList', 'numberedList', 'blockQuote'],
+            heading: {
+                options: [
+                    {model: 'paragraph', title: 'Paragraph', class: 'ck-heading_paragraph'},
+                    {model: 'heading1', view: 'h1', title: 'Heading 1', class: 'ck-heading_heading1'},
+                    {model: 'heading2', view: 'h2', title: 'Heading 2', class: 'ck-heading_heading2'},
+                    {model: 'heading3', view: 'h3', title: 'Heading 3', class: 'ck-heading_heading3'}
+                ]
+            }
+        })
+        .then(editor => {
+            editor.setData("What's in your mind today?");
+
+            $(".ck-editor").addClass('col-12');
+
+            $('#submit').on('click', () => {
+                const editorData = editor.getData();
+
+                // TODO: send the POST package to /api/post
+                console.log(editorData);
+            });
+        })
+        .catch(error => {
+            console.error(error);
+        });
+})
