34dc6db2d420b91d5edc35150119059f03865c4d
genglinliu <44108482+genglinliu@users.noreply.github.com>
2021-04-25 12:11:22 -0400

    initial attempt - blogpage backend

--- routes/api/blogpage_backend.js
+++ routes/api/blogpage_backend.js
@@ -3,10 +3,25 @@ const db = require(appRoot + '/db')
 const router = new Router();
 
 // TODO: handle the two requests and insert them into database
-
-// click on "like" - add 'this' user and 'this blog' to the sql table 'liked'
-
-// click on "follow" - add 'this' user and 'this blogger' to the sql table 'follow'
+router
+    // click on "like" - add 'this' user and 'this blog' to the sql table 'liked'
+    .post('/blogpage_backened', async (req, res) => {
+        console.log('here');
+        let user_id = req.body.user_id;
+        let post_id = req.body.post_id;
+        let result = await db.query("INSERT INTO like VALUES ($1, $2);", [user_id, post_id]);
+        
+        console.log("result:", JSON.stringify(result.rows));
+    })
+    // click on "follow" - add 'this' user and 'this blogger' to the sql table 'follow'
+    .post('/blogpage_backened', async (req, res) => {
+        console.log('here');
+        let this_user_id = req.body.this_user_id;
+        let poster_user_id = req.body.poster_user_id;
+        let result = await db.query("INSERT INTO like VALUES ($1, $2);", [this_user_id, poster_user_id]);
+        
+        console.log("result:", JSON.stringify(result.rows));
+    })
 
 
 
