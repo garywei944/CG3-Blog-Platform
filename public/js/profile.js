80b1f86dcb60e6fb8056f0f49f9649467450cab4
garywei944 <33930674+garywei944@users.noreply.github.com>
2021-04-25 23:48:40 -0400

    Finish Follower and Following on profile

--- public/js/profile.js
+++ public/js/profile.js
@@ -1,6 +1,6 @@
 let post_card_str = `
     <% posts.reverse().forEach(function(post) { %>
-        <div id="<%= post.post_id %>" class="card mt-4 posts">
+        <div post_id="<%= post.post_id %>" class="card mt-4 posts">
             <div class="card-body btn btn-outline-dark">
                 <h5 class="card-title"><%= post.title %></h5>
                 <p class="card-text"><%= post.content %></p>
@@ -10,20 +10,33 @@ let post_card_str = `
     <% }); %>
 `;
 
-let post_render = ejs.compile(post_card_str, {});
+let follower_item_str = `
+    <% users.forEach(function(user) { %>
+        <a username="<%= user.follower_name %>" class="dropdown-item" href="/<%= user.follower_name %>"><%= user.follower_name %></a>
+    <% }); %>
+`;
+let following_item_str = `
+    <% users.forEach(function(user) { %>
+        <a username="<%= user.username %>" class="dropdown-item" href="/<%= user.username %>"><%= user.username %></a>
+    <% }); %>
+`;
 
 
 $(function () {
+    let post_render = ejs.compile(post_card_str, {});
+    let follower_render = ejs.compile(follower_item_str, {});
+    let following_render = ejs.compile(following_item_str, {});
+
     let username = $(location).attr('pathname').match(/.*\/(.*)/)[1];
 
     $.get('/api/' + username + '/posts', function (data) {
-        let html = post_render({
+        if (data.length === 0) return;
+        $("#posts").empty().html(post_render({
             posts: data
-        });
-        $("#posts").empty().html(html);
+        }));
     }, 'json').done(function () {
         $(".posts").on('click', function () {
-            window.location.href = '/post/' + $(this).attr('id');
+            $(location).attr('href', '/post/' + $(this).attr('post_id'));
         });
     });
 
@@ -34,4 +47,23 @@ $(function () {
     } else {
         // TODO:
     }
+
+    // Render Follower and Following dropdown list
+    $.get('/api/' + username + '/follower', function (data) {
+        if (data.length === 0) return;
+        $("#follower_dropdown").empty().html(follower_render({
+            users: data
+        }));
+    }, 'json').done(function () {
+        $("#follower_dropdown>a").href('/post/' + $(this).attr('username'));
+    });
+
+    $.get('/api/' + username + '/following', function (data) {
+        if (data.length === 0) return;
+        $("#following_dropdown").empty().html(following_render({
+            users: data
+        }));
+    }, 'json').done(function () {
+        $("#following_dropdown>a").href('/post/' + $(this).attr('username'));
+    });
 })
