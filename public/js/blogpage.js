52d72b0c66e13f856bea16d99d19188e529b0a31
genglinliu <44108482+genglinliu@users.noreply.github.com>
2021-04-26 13:10:31 -0400

    follow button implemented

--- public/js/blogpage.js
+++ public/js/blogpage.js
@@ -6,17 +6,15 @@ $(document).ready(function() {
     // let c_username = checkCookie("cg3");
     let c_username = Cookies.get("cg3");
     console.log(c_username)
+
     // click on "like" - add 'this' user and 'this blog' to the database table 'liked'
     $('#like').click(function(){                
-
-        console.log("like button clicked")
-
         const input = {
             user_id: c_username,
             post_id: post_id
         };
     
-        console.log(input);
+        alert(c_username + " liked post " + post_id)
     
         $.ajax({
             type: "POST",
@@ -24,7 +22,7 @@ $(document).ready(function() {
             data: input,
         })
         .done(function(data) {
-            console.log(user_id + " liked post " + post_id)
+            // success
         })
         .fail(function(jqXHR) {
             console.log("failed to post like");
@@ -33,16 +31,13 @@ $(document).ready(function() {
 
     // click on "follow" - add 'this' user and 'this blogger' to the database table 'follows'
     $("#follow").click(function() {
-        console.log("follow button clicked")
-
         const input = {
             this_user_id: c_username,
-            poster_user_id: "Devin"
+            post_id: post_id,
+            // we send post_id to backend and let it query for the poster_user
         };
-
-        // $.get("/api/blogpage_poster", function(data, status){
-
-        // })
+        
+        alert(c_username + " followed user ")
         
         $.ajax({
             type: "POST",
@@ -50,7 +45,7 @@ $(document).ready(function() {
             data: input,
         })
         .done(function(data) {
-            console.log(this_user_id + " followed user " + poster_user_id)
+            // success
         })
         .fail(function(jqXHR) {
             console.log("failed to post follow");
--- routes/api/blogpage_backend.js
+++ routes/api/blogpage_backend.js
@@ -11,27 +11,31 @@ router
         let result = await db.query("INSERT INTO liked VALUES ($1, $2);", [user_id, post_id]);
 
         console.log(user_id + " liked post " + post_id)
+        res.sendStatus(201);
     })
     // click on "follow" - add 'this' user and 'this blogger' to the sql table 'follow'
     .post('/blogpage_backened_follow', async (req, res) => {
         let this_user_id = req.body.this_user_id;
-        let poster_user_id = req.body.poster_user_id;
+        let post_id = req.body.post_id;
 
-        // 这里反了吧...  database里是 username | follower_name
-        // let result = await db.query("INSERT INTO follow VALUES ($1, $2);", [this_user_id, poster_user_id]);
-        let result = await db.query("INSERT INTO follow VALUES ($1, $2);", [poster_user_id, this_user_id]);
+        const result = await db.query("select username from post where post_id = $1", [post_id]);
+        let poster_user_id = (result) ? result.rows : null; // [ { username: 'test4@test' } ]
+        poster_user_id = poster_user_id[0].username; // 'test4@test'
+        let result2 = await db.query("INSERT INTO follow VALUES ($1, $2);", [this_user_id, poster_user_id]);
 
         console.log(this_user_id + " followed user " + poster_user_id)
 
         res.sendStatus(201);
     })
-// .get('/blogpage_poster', async (req, res) => {
-//     const post_id = req.params.post_id;
-//     const result = await db.query("select username from post where post_id = $1", [post_id]);
-//     const results = (result) ? result.rows : null;
 
-//     res.json(results)
-// });
+    .get('/blogpage_poster', async (req, res) => {
+        console.log(req)
+        const post_id = req.params.post_id;
+        const result = await db.query("select username from post where post_id = $1", [post_id]);
+        const results = (result) ? result.rows : null;
+
+        res.json(results);
+    });
 
 
 module.exports = router;
